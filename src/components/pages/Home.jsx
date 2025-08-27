45e260a0cb1c2154168f517f6bd176cbe77cf0bf
Titus Rangga <titusrangga142@gmail.com>
2025-08-27 09:28:22 +0700

    fix: modify import Upcoming

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -5,7 +5,7 @@ import { fetchAllMoviesThunk } from '../../redux/movie/moviesSlice';
 import Hero from '../templates/Home/Hero'
 import ChooseUs from '../templates/Home/ChooseUs'
 import Trending from '../templates/Home/Trending'
-import UpComing from '../templates/Home/UpComing'
+import UpComing from '../templates/Home/UpComing.jsx'
 import Subscribe from '../templates/Subscribe'
 
 function Home() {
